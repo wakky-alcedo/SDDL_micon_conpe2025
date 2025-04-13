fc6bbcb4561a17961fc3a2720df70adcbd2ff798
wakky-alcedo <wakky.alcedo@gmail.com>
2025-04-13 15:51:19 +0900

    [1st_lab] add: 音楽

--- 1st_lab/src/main.cpp
+++ 1st_lab/src/main.cpp
@@ -3,10 +3,16 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <WiFiUdp.h>
+#include "DFRobotDFPlayerMini.h"
+#include <SoftwareSerial.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 // Wi-Fiの設定
 const char* ssid = "SDDLnet";
 const char* password = "smallbear";
+// const char* ssid = "Hippopotamus";
+// const char* password = "origami2827";
 
 // MQTTブローカーの設定
 const char* mqtt_server = "192.168.0.206"; // MQTTブローカーのIPアドレス todo
@@ -31,7 +37,7 @@ unsigned long startTime = 0;
 // constexpr int lightSensorVccPin = 22; // ライトセンサーのピン番号を定義 (GPIO34を使用)
 constexpr int lightSensorPin = 35
 ; // ライトセンサーのピン番号を定義 (GPIO27を使用)
-constexpr uint8_t threshold = 100; // ライトセンサーのしきい値を定義 (適宜調整してください)
+constexpr uint8_t threshold = 20; // ライトセンサーのしきい値を定義 (適宜調整してください)
 constexpr uint8_t callLedPin = 4; // ライトセンサーのVccピンを定義 (GPIO22を使用)
 
 String senderIP = ""; // 送信者のIPアドレスを格納する変数
@@ -48,12 +54,22 @@ byte pcmBuffer[packetSize];
 
 bool isCalling = false; // 通話中かどうかのフラグ
 
+SoftwareSerial mySoftwareSerial(22, 21); // RX, TX
+HardwareSerial myHardwareSerial(2); // use HardwareSerial UART1
+DFRobotDFPlayerMini myDFPlayer;
+
+const String timeApiUrl = "https://www.timeapi.io/api/Time/current/zone?timeZone=Asia/Tokyo";
+constexpr uint16_t eveningChimeTime = 17 * 60; // 17:00 (17時) の時間を分単位で定義
+constexpr uint16_t workingEndChimeTime = 20 * 60; // 20:00 (20時) の時間を分単位で定義
+
 // 関数のプロトタイプ宣言
 void setup_wifi();
 void callback(char* topic, byte* payload, unsigned int length);
 void reconnect();
 void publishStatus(bool lightStatus = false);
 void publishTopic(const char* topic, String statusMessage);
+int setTimeFromApi();
+void printTime();
 
 void setup_wifi() {
   delay(10);
@@ -143,36 +159,127 @@ void publishTopic(const char* topic, String statusMessage) {
   Serial.println(statusMessage);
 }
 
+int setTimeFromApi() {
+  // Wi-Fi接続確認
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Wi-Fi接続が切断されました。再接続中...");
+    WiFi.begin(ssid, password);
+    return -1; // エラー
+  }
+  
+  HTTPClient http;
+  
+  Serial.print("APIに接続中...: ");
+  Serial.println(timeApiUrl);
+  
+  // HTTP開始
+  http.begin(timeApiUrl);
+  
+  // GETリクエスト送信
+  int httpResponseCode = http.GET();
+
+  http.end(); // HTTP終了
+  
+  if (httpResponseCode > 0) {
+    Serial.print("HTTPレスポンスコード: ");
+    Serial.println(String(httpResponseCode));
+    
+    String payload = http.getString();
+    
+    // JSONレスポンスを解析
+    DynamicJsonDocument doc(1024);
+    DeserializationError error = deserializeJson(doc, payload);
+    
+    if (error) {
+      Serial.print("JSON解析エラー: ");
+      Serial.println(error.c_str());
+    } else {
+      // 時間をマイコンの時間に設定 clock_settimeを使用
+      struct tm timeInfo;
+      timeInfo.tm_year = doc["dateTime"].as<String>().substring(0, 4).toInt() - 1900; // 年を設定 (1900年からの経過年数)
+      timeInfo.tm_mon = doc["dateTime"].as<String>().substring(5, 7).toInt() - 1; // 月を設定 (0-11の範囲)
+      timeInfo.tm_mday = doc["dateTime"].as<String>().substring(8, 10).toInt(); // 日を設定 (1-31の範囲)
+      timeInfo.tm_hour = doc["dateTime"].as<String>().substring(11, 13).toInt(); // 時を設定 (0-23の範囲)
+      timeInfo.tm_min = doc["dateTime"].as<String>().substring(14, 16).toInt(); // 分を設定 (0-59の範囲)
+      timeInfo.tm_sec = doc["dateTime"].as<String>().substring(17, 19).toInt(); // 秒を設定 (0-59の範囲)
+      timeInfo.tm_isdst = -1; // 夏時間の設定 (自動判定)
+      time_t epochTime = mktime(&timeInfo); // 時刻をエポック時間に変換
+      struct timespec ts;
+      ts.tv_sec = epochTime; // 秒を設定
+      ts.tv_nsec = 0; // ナノ秒を設定 (0に設定)
+      clock_settime(CLOCK_REALTIME, &ts); // システム時刻を設定
+      return 0; // 成功
+    }
+  } else {
+    Serial.print("HTTP GET エラー: ");
+    Serial.println(String(httpResponseCode));
+  }
+  return -1; // エラー
+}
+
+void printTime() {
+  time_t now = time(nullptr);
+  struct tm* timeInfo = localtime(&now);
+  Serial.print("Current time: ");
+  Serial.println(String(timeInfo->tm_hour) + ":" + String(timeInfo->tm_min) + ":" + String(timeInfo->tm_sec));
+}
+
 void setup() {
   Serial.begin(115200);
+  delay(100);
+
+  // Wi-Fi接続
   setup_wifi();
+  // MQTT接続
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
-  startTime = millis();
+  reconnect();
 
-  // pinMode(lightSensorVccPin, OUTPUT); // ライトセンサーのVccピンを出力に設定
-  // digitalWrite(lightSensorVccPin, HIGH); // ライトセンサーのVccピンをHIGHに設定
+  // 時刻を設定
+  while (setTimeFromApi() != 0) {
+    Serial.println("時刻の設定に失敗しました。再試行中...");
+    delay(5000); // 5秒待機
+  }
+  printTime(); // 時刻を表示
+
+  // ピンの設定
   pinMode(lightSensorPin, INPUT); // ライトセンサーの入力ピンを設定
   pinMode(callLedPin, OUTPUT); // 通話LEDピンを出力に設定
   digitalWrite(callLedPin, HIGH); // 通話LEDを初期状態で点灯
   dacWrite(speakerPin, 0);
+
+  // DFPlayer Miniの初期化
+  Serial.println("MP3 Player");
+  mySoftwareSerial.begin(9600);
+  // myHardwareSerial.begin(9600, SERIAL_8N1, 22, 21); // RX, TX
+
+  while(!myDFPlayer.begin(mySoftwareSerial)) { //Use software serial to communicate with mp3 module
+    Serial.print(".");
+    delay(500);
+  }
+  myDFPlayer.volume(20); // Set volume value (0-30).
+  Serial.println("MP3 Player ready.");
+  // myDFPlayer.play(1);  //Play the first mp3
+  Serial.println("Playing track 1.");
 }
 
 void loop() {
-  if (!client.connected()) {
-    long now = millis();
-    if (now - lastReconnectAttempt > 5000) {
-      lastReconnectAttempt = now;
-      reconnect();
-    }
-  }
-  client.loop();
+  static uint16_t slowLoopCount = 0; // statusを表示用のカウンタ
+  slowLoopCount++;
+  if (slowLoopCount >= 1000) { // ADCの処理頻度が高いと値が安定しない
 
-  // ライトセンサー
-  static uint16_t showCount = 0; // statusを表示用のカウンタ
-  showCount++;
-  if (showCount >= 500) { // ADCの処理頻度が高いと値が安定しない
-    showCount = 0;
+    // MQTT接続確認
+    if (!client.connected()) {
+      long now = millis();
+      if (now - lastReconnectAttempt > 5000) {
+        lastReconnectAttempt = now;
+        reconnect();
+      }
+    }
+    client.loop();
+
+    // ライトセンサー
+    slowLoopCount = 0;
     uint8_t lightValue = analogRead(lightSensorPin);
     Serial.println("Light Sensor Value: " + String(lightValue));
     static bool prevLightStatus = false; // 前回のライトの状態を保存する変数
@@ -180,6 +287,27 @@ void loop() {
       publishStatus(lightValue > threshold);
     }
     prevLightStatus = lightValue > threshold; // 現在の状態を保存
+
+    // 時刻を取得して、チャイムを鳴らす
+    time_t now = time(nullptr);
+    struct tm* timeInfo = localtime(&now);
+    uint16_t currentTime = timeInfo->tm_hour * 60 + timeInfo->tm_min; // 現在の時間を分単位で取得
+    static uint16_t prevTime = 0; // 前回の時間を保存する変数
+    if (prevTime != currentTime) {
+      Serial.println("Current time: " + String(timeInfo->tm_hour) + ":" + String(timeInfo->tm_min) + ":" + String(timeInfo->tm_sec));
+      if (currentTime == eveningChimeTime) {
+        Serial.println("Evening chime time!");
+        myDFPlayer.play(1); // 1番の音声を再生
+      } else if (currentTime == workingEndChimeTime) {
+        Serial.println("Working end chime time!");
+        myDFPlayer.play(2); // 2番の音声を再生
+        Serial.println("Working end chime time!");
+      } else if (currentTime == 0) {
+        setTimeFromApi(); // 0時になったらAPIから時刻を取得、再設定
+        Serial.println("Time reset from API.");
+      }
+      prevTime = currentTime; // 現在の時間を保存
+    }
   }
 
   // 通話中の処理
@@ -196,9 +324,9 @@ void loop() {
         dacWrite(speakerPin, dacValue);
         delayMicroseconds(1000000 / sampleRate);
       }
-      Serial.printf("Received packet of size %d\n", packetSizeReceived);
+      Serial.println("Received packet of size " + String(packetSizeReceived));
     } else if (packetSizeReceived > 0) {
-      Serial.printf("Received unexpected packet size: %d\n", packetSizeReceived);
+      Serial.println("Received packet of unexpected size: " + String(packetSizeReceived));
     }
   }
   delay(1);
